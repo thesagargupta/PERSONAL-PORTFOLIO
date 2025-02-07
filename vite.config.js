249dc762da23a6a8bffa988521925c8813d32e39
sagar gupta <sagarkshn8@gmail.com>
2025-02-07 23:10:58 +0530

    commit fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Allows access from network
-    port: 3000, // You can change this if needed
+    host: "0.0.0.0", // Allow access to the dev server from other devices
+    port: 3000, // You can set a specific port if needed
+    proxy: {
+      "/api": {
+        target: "https://portfolio-backend-g3vi.onrender.com/",
+        changeOrigin: true,
+      },
+    },
   },
-})
+});
